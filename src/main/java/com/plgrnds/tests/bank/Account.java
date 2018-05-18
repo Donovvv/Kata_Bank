c000433b8af816c59a98b03a81d7364a76214511
MrBadExample <rostek.szymon@gmail.com>
2018-05-18 19:56:25 +0200

    Update Account.java

--- src/main/java/com/plgrnds/tests/bank/Account.java
+++ src/main/java/com/plgrnds/tests/bank/Account.java
@@ -19,6 +19,10 @@ public class Account {
     public Customer getOwner() {
         return owner;
     }
+    
+    public bigDecinal getBalance() {
+        return this.balance;
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -39,7 +43,33 @@ public class Account {
     public void setBalance(double balance){
         this.balance = balance;
     }
+public void withdraw(BigDecimal amount) throws IllegalArgumentException {
+        checkValidAmount(amount);
+        if (amount.compareTo(this.balance) > 0) {
+            throw new IllegalArgumentException("The amount to withdraw is greater than account balance.");
+        }
+        this.balance = this.balance.subtract(amount);
+    }
 
+    public void deposit(BigDecimal amount) {
+        checkValidAmount(amount);
+        this.balance = this.balance.add(amount);
+    }
+
+    private void checkValidAmount(BigDecimal amount) {
+        if (amount == null || amount.signum() <= 0) {
+            throw new IllegalArgumentException("Incorrect amount to process: "
+                    + (amount == null ? null : amount.toString()));
+        }
+    }
+
+    public void transfer(Account targetAccount, BigDecimal bigDecimal) {
+        if (targetAccount == null) {
+            throw new RuntimeException("Target account cannot be null.");
+        }
+        this.withdraw(bigDecimal);
+        targetAccount.deposit(bigDecimal);
+    }
 
 
 }
