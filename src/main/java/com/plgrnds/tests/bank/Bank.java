1aae0805a1a9c44cc77dd2aaf790279ef61e041b
MrBadExample <rostek.szymon@gmail.com>
2018-05-18 20:01:19 +0200

    Update Bank.java

--- src/main/java/com/plgrnds/tests/bank/Bank.java
+++ src/main/java/com/plgrnds/tests/bank/Bank.java
@@ -13,9 +13,35 @@ public class Bank {
 	public Set<Account> getCustomerAccounts(Customer customer) {
 		return accountRepository.getAccountsForCustomer(customer);
 	}
+	
+	public void deposit(Customer customer, Account account, BigDecimal depositAmount) {
+        checkOperationNotAllowed(customer, account, ERROR_MESSAGE_DEPOSIT);
+        account.deposit(depositAmount);
+    }
+
+        public void withdraw(Customer customer, Account account, BigDecimal withdrawAmount) {
+        checkOperationNotAllowed(customer, account, ERROR_MESSAGE_WITHDRAW);
+        account.withdraw(withdrawAmount);
+    }
 	public void transfer(Account firstAccount, Account secoundAccount, double transferAmount) {
 		firstAccount.setBalance(); = firstAccount.getBalance(); - transferAmount;
 		secoundAccount.setBalance(); = secoundAccount.getBalance(); + transferAmount;
 	}
+	
+	
+        private void checkOperationNotAllowed(Customer customer, Account account, String message) {
+        if (!getAccountsForCustomer(customer).contains(account)) {
+            throw new RuntimeException(message);
+        }
+    }
+
+  	public Set<Deposit> getDepositsForCustomer(Customer customer) {
+        return bankProductsRepository.findDepositsForCustomer(customer);
+    }
+
+        public Deposit openDeposit (Customer customer, Account account, BigDecimal depositBalance, DepositType depositType){
+        checkOperationNotAllowed(customer, account, ERROR_MESSAGE_OPEN_DEPOSIT);
+        return new Deposit(account, depositBalance, depositType);
+    }
 
 }
